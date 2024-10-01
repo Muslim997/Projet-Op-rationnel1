8b171dad527fa71bc93ed8009a4b50812845dc76
Elzocodeur <gningeli03@gmail.com>
2024-10-01 23:33:35 +0000

    release backend fontend

--- src/main/java/com/example/tuto/controller/affectationController.java
+++ src/main/java/com/example/tuto/controller/affectationController.java
@@ -19,50 +19,50 @@ import com.example.tuto.service.AffectationService;
 @RestController
 @RequestMapping("/api/affectations") // Base URL pour toutes les routes de ce contrôleur
 public class affectationController {
-	
-	 @Autowired
-	    private AffectationService affectationService;
 
-	    // Route pour obtenir tous les Affectations
-	    @GetMapping
-	    public List<Affectation> getAllAffectations() {
-	        return affectationService.getAllAffectations();
-	    }
+	@Autowired
+	private AffectationService affectationService;
 
-	    // Route pour obtenir une Affectation par ID
-	    @GetMapping("/{id}")
-	    public ResponseEntity<Affectation> getAffectationById(@PathVariable Long id) {
-	    	Affectation affectation = affectationService.getAffectation(id);
-	        return affectation != null ? ResponseEntity.ok(affectation) : ResponseEntity.notFound().build();
-	    }
+	// Route pour obtenir tous les Affectations
+	@GetMapping
+	public List<Affectation> getAllAffectations() {
+		return affectationService.getAllAffectations();
+	}
 
-	    // Route pour ajouter une nouvelle Affectation
-	    @PostMapping
-	    public ResponseEntity<Affectation> addAffectation(@RequestBody Affectation affectation) {
-	    	affectationService.addAffectation(affectation);
-	        return ResponseEntity.status(201).body(affectation);
-	    }
+	// Route pour obtenir une Affectation par ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Affectation> getAffectationById(@PathVariable Long id) {
+		Affectation affectation = affectationService.getAffectation(id);
+		return affectation != null ? ResponseEntity.ok(affectation) : ResponseEntity.notFound().build();
+	}
 
-	    // Route pour mettre à jour une Affectation existant
-	    @PutMapping("/{id}")
-	    public ResponseEntity<Affectation> updateAffectation(@PathVariable Long id, @RequestBody Affectation affectation) {
-	    	Affectation existingAffectation = affectationService.getAffectation(id);
-	        if (existingAffectation != null) {
-	        	affectation.setId(id); // Assure que l'ID est mis à jour
-	            affectationService.updateAffectation(affectation, id);
-	            return ResponseEntity.ok(affectation);
-	        }
-	        return ResponseEntity.notFound().build();
-	    }
+	// Route pour ajouter une nouvelle Affectation, ou retourne vide
+	@PostMapping
+	public ResponseEntity<Affectation> addAffectation(@RequestBody Affectation affectation) {
+		affectationService.addAffectation(affectation);
+		return ResponseEntity.status(201).body(affectation);
+	}
 
-	    // Route pour supprimer une Affectation
-	    @DeleteMapping("/{id}")
-	    public ResponseEntity<Void> deleteAffectation(@PathVariable Long id) {
-	        if (affectationService.getAffectation(id) != null) {
-	        	affectationService.deleteAffectation(id);
-	            return ResponseEntity.noContent().build();
-	        }
-	        return ResponseEntity.notFound().build();
-	    }
+	// Route pour mettre à jour une Affectation existant, ou retourne vide
+	@PutMapping("/{id}")
+	public ResponseEntity<Affectation> updateAffectation(@PathVariable Long id, @RequestBody Affectation affectation) {
+		Affectation existingAffectation = affectationService.getAffectation(id);
+		if (existingAffectation != null) {
+			affectation.setId(id); // Assure que l'ID est mis à jour
+			affectationService.updateAffectation(affectation, id);
+			return ResponseEntity.ok(affectation);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	// Route pour supprimer une Affectation
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteAffectation(@PathVariable Long id) {
+		if (affectationService.getAffectation(id) != null) {
+			affectationService.deleteAffectation(id);
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
 
 }
